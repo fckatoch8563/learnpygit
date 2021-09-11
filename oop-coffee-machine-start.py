7d8f2c4c182495069c0d89938e8e207482ccaa12
F C Katoch <fckatoch45@gmail.com>
2021-09-11 13:41:12 +0530

    one line added

--- oop-coffee-machine-start.py
+++ oop-coffee-machine-start.py
@@ -1,1 +1,31 @@
+print("Coffee Machine OOP Version")
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+is_on = True
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+while is_on:
+    option = menu.get_items()
+    choice = input(f"What would you like? ({option}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        # if coffee_maker.is_resource_sufficient(drink):
+        # if money_machine.make_payment(drink.cost) & money_machine.make_payment(drink.cost):
+        # coffee_maker.make_coffee(drink)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if is_enough_ingredients & is_payment_successful:
+            coffee_maker.make_coffee(drink)
+
+
+
 
